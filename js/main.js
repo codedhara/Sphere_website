fb4b41f0e21a13cb7c733af1c815e04e5ca13376
Chaitanya Kolwankar <chaitanyakolwankar@gmail.com>
2024-10-06 02:58:17 +0530

    completion of index page

--- js/main.js
+++ js/main.js
@@ -139,6 +139,13 @@
         return false;
     });
 
+    
 
 })(jQuery);
 
+//--clients
+var copy = document.querySelector(".logos-slide").cloneNode(true);
+document.querySelector(".logo-slider").appendChild(copy);
+
+
+  
